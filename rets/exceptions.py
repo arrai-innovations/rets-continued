24bbb04b1cfaffb48f5157e0ab22483a283106f3
Peter Mack <peter@arrai.com>
2026-03-12 13:35:28 -0600

    chore: noqa won'tfix lint issues

--- rets/exceptions.py
+++ rets/exceptions.py
@@ -1,4 +1,4 @@
-class RETSException(Exception):
+class RETSException(Exception):  # noqa: N818
     """The RETS Server Returned an Issue"""
 
     def __init__(self, reply_text=None, reply_code=None):
@@ -12,11 +12,11 @@ class RETSException(Exception):
         return self.reply_text
 
 
-class HTTPException(Exception):
+class HTTPException(Exception):  # noqa: N818
     pass
 
 
-class NotLoggedIn(Exception):
+class NotLoggedIn(Exception):  # noqa: N818
     """Authentication Required to Access RETS server"""
 
     pass
@@ -28,13 +28,13 @@ class ParseError(Exception):
     pass
 
 
-class MissingVersion(Exception):
+class MissingVersion(Exception):  # noqa: N818
     """The RETS Version is required"""
 
     pass
 
 
-class MaxrowException(Exception):
+class MaxrowException(Exception):  # noqa: N818
     """The RETS Servers truncated the results with a <MAXROW/>"""
 
     def __init__(self, rows_returned):
