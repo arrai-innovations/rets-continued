58848286e9f62375edea1719c2f1780ab91d5951
Peter Mack <peter@arrai.com>
2026-03-12 15:57:10 -0600

    chore: bump version for release

--- rets/__init__.py
+++ rets/__init__.py
@@ -2,8 +2,8 @@ from .exceptions import RETSException
 from .session import Session
 
 
-__title__ = "rets"
-__version__ = "1.0.0"
+__title__ = "rets-continued"
+__version__ = "2.0.0"
 __author__ = ["REfindly", "Arrai Innovations"]
 __license__ = "MIT"
 __copyright__ = ["Copyright 2019 REfindly", "Copyright 2026 Arrai Innovations"]
